f837a5b126b066479e45d2be3b19899adce11d92
Tony-Freire <antoniowilliam.nave@gmail.com>
2022-09-18 22:13:26 -0300

    recebendo info dos livros

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import  express  from "express";
-import data from "./data.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import db from './db.js';
 
-import getBooks from "./controllers/booksController.js";
+import {getBooks, getBookInfo} from "./controllers/booksController.js";
+
 
 dotenv.config();
 const port = process.env.PORT || 5000;
-const app = express();
-app.use([cors(), express.json()]);
+const server = express();
+server.use([cors(), express.json()]);
 
+server.get('/products',getBooks);
+server.get('/products/slug',getBookInfo)
 
-app.get('/products',getBooks);
-
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Servidor rodando na porta ${port}`)});
